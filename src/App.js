9b05a97ef8cc54b7d8a056881c27993c7de77b05
magn01ia <84883260+magn01ia@users.noreply.github.com>
2022-01-31 14:29:15 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer} from "react-leaflet";
 import { LatLng } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./App.css";
