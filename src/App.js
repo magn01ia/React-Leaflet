34fe16e510e65414ed0938bdb22429157033e2e2
magn01ia <84883260+magn01ia@users.noreply.github.com>
2022-01-31 13:33:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import { LatLng } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import "./App.css";
 
 function App() {
+  const position = new LatLng(36.558894, 139.898347);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={position} zoom={10}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   );
 }
