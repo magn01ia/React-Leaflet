8f400b38b589ef886269c023fcabf886637565fb
magn01ia <84883260+magn01ia@users.noreply.github.com>
2022-01-31 14:21:58 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { LatLng } from "leaflet";
 import "leaflet/dist/leaflet.css";
-
 import "./App.css";
 
 function App() {
   const position = new LatLng(36.558894, 139.898347);
-
   return (
     <div className="App">
-      <MapContainer center={position} zoom={10}>
+      <MapContainer center={position} zoom={8}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
